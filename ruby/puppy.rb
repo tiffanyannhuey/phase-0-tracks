14a1ac38abc0649acb18d380f61ec5890496b8b8
tiffanyannhuey <87tiffanyhuey@gmail.com>
2016-12-20 21:08:40 -0800

    Design a Puppy Class

---
+++ ruby/puppy.rb
@@ -0,0 +1,17 @@
+# SPECIES ------------------------------------------
+# Gigantus Flufferdome
+
+# CHARACTERISTICS -----------------------------------
+# Size: ranges from 130-200 lbs 
+# Appearance: cloud-like
+# Fur texture: soft
+# Friendly: yes
+# Paws: 4
+# Name: varies
+# Color: white
+
+# BEHAVIOR -------------------------------------------
+
+# Cuddles
+# Sleeps
+# Eats
