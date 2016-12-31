03fd263e587795ac9f9cfd38f12edae247a71c9d
tiffanyannhuey <87tiffanyhuey@gmail.com>
2016-12-30 19:04:11 -0800

    add Superhero class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -28,9 +28,46 @@ class Puppy
 
 end
 
+#Driver Code
 fido = Puppy.new
 fido.fetch("ball")
 fido.speak(5)
 fido.roll_over
 fido.dog_years(5)
 fido.beg("biscuits")
+
+
+class Superhero
+
+  def initialize
+    puts "Initializing new Superhero class"
+  end
+
+  def beat
+    puts "Look! Its a superhero!!!!"
+    puts "He is BEATING THE BAD GUYS!!"
+  end
+
+  def save(number)
+      unless number == 1 then puts "He just saved #{number} damsels in distress!!!!" 
+      else 
+      puts "He just saved a damsel in distress!!!"
+    end 
+  end
+
+end 
+
+Justice_league = []
+
+50.times.each do |hero|
+  Justice_league << hero = Superhero.new
+end 
+
+Justice_league.each do |hero|
+  hero.beat 
+  hero.save(1)
+end 
+
+#Driver Code 
+p Justice_league
+
