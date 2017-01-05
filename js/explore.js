26c8bb92b277fa6c80fb7c8850c5fad0957c4fef
tiffanyannhuey <87tiffanyhuey@gmail.com>
2017-01-04 23:01:51 -0800

    add If statement to print when condition true

--- js/explore.js
+++ js/explore.js
@@ -20,6 +20,7 @@ return reversedString;
 
 //Driver code to test the function
 
+// Prompt the user for a word
 console.log("Give me a word to reverse");
 var userWord = prompt("Enter a word.");
 // assign the result of the function to a new variable
@@ -30,4 +31,19 @@ console.log("The new word is " + result);
 
 
 
+// Add driver code that prints out the variable if condition is true
 
+
+// create two variables
+var num1 = 1;
+var num2 = 1;
+
+// make an IF statement 
+// if the condition is true, then print out the variables
+if (num1 === num2) {
+console.log( num1 + " and " + num2 + " are the same number");
+}
+// else if condition is false, do not print variables
+else {
+console.log("These variables are not the same ");
+}
