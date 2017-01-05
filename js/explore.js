d672d8fbb34834d74ac50748048309a8c1f0c73f
tiffanyannhuey <87tiffanyhuey@gmail.com>
2017-01-04 21:46:12 -0800

    Add function to pass in string and reverse

--- js/explore.js
+++ js/explore.js
@@ -1,15 +1,21 @@
 
 // Take a string as a parameter
+function reverse(string) {
+
+// create an empty string to add new letters to
+var reversedString = '';
+
 // For each letter in the string, print each letter starting with 
 //the last letter to the first, reversing the string
+// Loop through the length of the string until the index reaches 0
 
-
-
-
-
-
-var str = "hello";
-
-for(var i = str.length - 1; i >= 0; i --) {
-  console.log(str[i]);
+for(var i = string.length - 1; i >= 0; i --) {
+  reversedString += string[i];
 }
+// Print the reversed string
+console.log(reversedString);
+
+}
+
+//Driver code to test the function
+(reverse("Hello"));
