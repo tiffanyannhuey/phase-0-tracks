7f29fc7b1df852f78df2a50b8df0c5a02ecb544c
tiffanyannhuey <87tiffanyhuey@gmail.com>
2017-01-05 20:13:10 -0800

    add car constructor

--- js/data_structures.js
+++ js/data_structures.js
@@ -14,3 +14,20 @@ for (var i =0; i < colors.length; i ++ ) {
 }
 
 console.log(pairs);
+
+function Car(color, year, isRunning) {
+  this.color = color;
+  this.year = year;
+  this.isRunning = isRunning;
+  this.revEngine = function(){
+    console.log("*revs engine*");
+  }
+
+}
+
+var car1 = new Car('red', 2016, true);
+console.log(car1);
+
+car1.revEngine();
+console.log(car1.color);
+console.log(car1.year);
