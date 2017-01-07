8ea4f255b836887fcf266f001f9b449a75da03af
tiffanyannhuey <87tiffanyhuey@gmail.com>
2017-01-07 13:04:18 -0800

    Add pseudocode for large array sorting function

---
+++ js/algos.js
@@ -0,0 +1,5 @@
+// This function takes an array of words and then returns the longest of the words
+// The input would be an array
+// For every word in the array, evaluate the length of each word
+// Sort the length of each word from largest to smallest
+// Print out the largest word (first in the array)
