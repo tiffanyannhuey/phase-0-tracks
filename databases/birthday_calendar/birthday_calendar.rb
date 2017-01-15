2d744ddae3bd8c7c131ce239a601ab917af2ea51
tiffanyannhuey <87tiffanyhuey@gmail.com>
2017-01-14 20:30:59 -0800

    require gems for program

---
+++ databases/birthday_calendar/birthday_calendar.rb
@@ -0,0 +1,19 @@
+# BIRTHDAY CALENDAR
+
+# You can use this program to store the birthdays of your friends
+# You can see which friends are celebrating their birthday by typing in the month 
+
+# require gems
+require 'sqlite3'
+require 'faker'
+
+
+# create SQLite3 database
+
+# create a birthday table (if not there already)
+
+# add a test birthday
+
+# create a sort method that will retrieve by month
+
+# USER INTERFACE
