323e9d3cdb1d3dd225c9bac4a8a83c7cea8cb150
tiffanyannhuey <87tiffanyhuey@gmail.com>
2017-01-14 23:17:11 -0800

    Set database instance variable as hash

--- databases/birthday_calendar/birthday_calendar.rb
+++ databases/birthday_calendar/birthday_calendar.rb
@@ -10,13 +10,14 @@ require 'faker'
 
 # create SQLite3 database
 db = SQLite3::Database.new ("birthdays.db")
+db.results_as_hash = true
 
 # create fancy string
 create_table_cmd = <<-SQL
   CREATE TABLE IF NOT EXISTS birthdays(
   id INTEGER PRIMARY KEY,
   name VARCHAR(255),
-  month INT, 
+  month VARCHAR(255), 
   day INT
   )
 SQL
@@ -25,12 +26,14 @@ SQL
 db.execute(create_table_cmd)
 
 # add a test birthday
-# db.execute("INSERT INTO birthdays (name, month, day) VALUES ('Tiffany', 11, 10)")
+# db.execute("INSERT INTO birthdays (name, month, day) VALUES ('Tiffany', 'November', 10)")
 
 # explore ORM by retrieving data
 birthdays = db.execute("SELECT * FROM birthdays")
-puts birthdays.class
-p birthdays
+puts birthdays
+birthdays.each do |friend|
+  puts "#{friend['name']}'s birthday is on #{friend['month']} #{friend['day']}"
+end
 
 # create a sort method that will retrieve by month
 
